f6849edc71bc4b25a16c33b237687b006ff8f72d
Anastasia <nastya.k.r@mail.ru>
2017-11-09 23:56:01 +0300

    0.0

--- 07/03/task.js
+++ 07/03/task.js
@@ -1,9 +1,36 @@
+/* eslint-disable linebreak-style */
+
 /**
  * Исправьте проблему с таймером: должны выводиться числа от 0 до 9.
  * Доп. задание: предложите несколько вариантов решения.
  */
 function throttle(time, callback) {
-  return callback;
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function setPause(...args) {
+    if (isThrottled) {
+      savedArgs = args;
+      savedThis = this;
+      return;
+    }
+
+    callback.apply(this, args);
+
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+      if (savedArgs) {
+        setPause.apply(savedThis, savedArgs);
+        savedArgs = null;
+        savedThis = null;
+      }
+    }, time);
+  }
+
+  return setPause;
 }
 
 
